3939423d41b8c7b02958f94f44ec496b45c175d3
liuxinqi <liuxinqi@gridsum.com>
2022-01-18 15:06:45 +0800

    feat: 增加缓存设置

--- config/index.js
+++ config/index.js
@@ -2,5 +2,6 @@ module.exports={
     port:3000,
     defaultShowPage:"index.html",
     needCompressExtName:["img","js","html","png"],
-    cors:true
+    cors:true,
+    maxAge:3600
 }
--- handler.js
+++ handler.js
@@ -2,6 +2,7 @@ const fs=require("fs");
 const path=require("path")
 const {promisify}=require("util")
 const zlib=require("zlib")
+const crypto=require("crypto")
 
 const handleBars=require("handlebars");
 const mime =require("mime")
@@ -12,7 +13,7 @@ const readdir=promisify(fs.readdir)
 
 const defaultTemplate= handleBars.compile(require("./template/default_template"))
 // const defaultTemplate= handleBars.compile(require("./template/default_template"))
-const {defaultShowPage,needCompressExtName,cors}=require("./config")
+const {defaultShowPage,needCompressExtName,cors,maxAge}=require("./config")
 
 
 function handleNotFound(res){
@@ -21,21 +22,64 @@ function handleNotFound(res){
     rs.pipe(res)
 }
 
-function handleFile(req, res,pathName){
+async function handleFile(req, res,pathName ,stat){
     const extName = path.extname(process.cwd()+pathName).slice(1);
-  
+    const hash = crypto.createHash('sha1'); 
+    const lastModified= new Date(stat.mtime).toGMTString();
     let readStream=fs.createReadStream(process.cwd()+pathName);
-    res.setHeader('Content-Type',mime.getType(pathName)+';charset=utf-8')
-    if(cors){
-     res.setHeader('Access-Control-Allow-Origin', "*")
-    }
+    
+    // hash.update(readStream)
+    // readStream.pipe(hash) 
+    // const etag= hash.digest("hex")
+    readStream.on('data', (data) => {
+        hash.update(data);
+    });
+    readStream.on('end',async () => {
+        const etag = hash.digest('hex');
+        setCache(res,stat,etag)
+   
+        if(cors){
+            res.setHeader('Access-Control-Allow-Origin', "*")
+        }
+        
+       if(isShouldReturn304(req,lastModified,etag)){
+            res.statusCode=304;
+            return res.end();
+        }
+    
+        res.setHeader('Content-Type',mime.getType(pathName)+';charset=utf-8')
+       
+        //这里需要用新建的可读流，因为上个可读流已经流空
+        // let rs= fs.createReadStream(process.cwd()+pathName)
+    
+        let rs =  await getReadStream(req,res, pathName,stat)
+        if(isNeedCompress(extName)){
+            console.log("isNeedCompress");
+            rs = handleCompressFile(req, res,rs)
+        }
+    
+         rs.pipe(res)
 
-    if(isNeedCompress(extName)){
-        readStream = handleCompressFile(req, res,readStream)
-    }
-
-    readStream.pipe(res)
+    })
 }
+
+async function getReadStream(req,res, pathName,stat){
+    let start=0;
+    let end=stat.size-1
+    const range =  req.headers["range"]
+
+    if(range){
+        res.setHeader("Accept-Range","bytes");
+        res.statusCode=206;
+        const [rangeStart,rangeEnd] = req.headers["range"].split("=")[1].split("-");
+        start=isNaN(+rangeStart)?start:+rangeStart
+        end=isNaN(+rangeEnd)?end:+rangeEnd
+    }
+    
+   
+    return fs.createReadStream(process.cwd()+pathName,{ start, end })
+}
+
 
 async function handleDirectory(res,pathName,files){
     const fileList=[]
@@ -54,7 +98,69 @@ async function handleDirectory(res,pathName,files){
     res.end(result)
 }
 
+function isShouldReturn304(req,serverModifiedTime,etag){
+    const ifNoneMatch=req.headers["if-none-match"];
+    const ifModifiedSince=req.headers["if-modified-since"];
+    if(!ifNoneMatch&&!ifModifiedSince){
+       return false
+    }
+console.log("ifNoneMatch", ifNoneMatch);
+console.log("etag", etag);
+console.log("ifModifiedSince", ifModifiedSince);
+console.log("serverModifiedTime", serverModifiedTime);
+    if(ifNoneMatch!==etag&&+new Date(ifModifiedSince)!==+new Date(serverModifiedTime)){
+        return false
+    }
+    
 
+    return true
+}
+
+//设置缓存
+function setCache(res,stat,etag){
+    const lastModified= new Date(stat.mtime).toGMTString();
+    console.log(lastModified);
+    //设置一小时后过期
+    const expiresTime= new Date(+new Date()+60*60*1000).toGMTString();
+    //Cache-Control 强缓存http1.1 
+    //public：资源客户端和代理服务器都可以缓存。
+    //privite：资源只有客户端可以缓存。
+    //no-cache：客户端缓存资源，但是是否缓存需要经过协商缓存来验证。
+    //no-store：不使用缓存。
+    //max-age：缓存保质期。
+    res.setHeader('Cache-Control', `public, max-age=${maxAge}`);
+    //expires 强缓存http1.0; 
+    //本地GMT时间格式,但是本地时间可以自己修改
+    res.setHeader('expires', expiresTime);
+    
+    //Last-Modified 协商缓存 http1.0 
+    //服务器响应给客户端文件的最后修改日期，下次请求时请求头携带if-modified-since字段值为上次请求返回的Last-Modified值.
+    res.setHeader('Last-Modified', lastModified);
+
+     //Etag 协商缓存 http1.1 
+    //服务器响应给客户端能表示该文件的唯一标识，下次请求时请求头携带if-modified-since字段值为上次请求返回的Etag值.
+    res.setHeader('Etag', etag);
+
+}
+
+
+async function handleCache(req,res){
+    //强缓存
+    //响应头中expires、pragma或者cache-control字段，代表这是强缓存;浏览器就会把资源缓存在memory cache 或 disk cache中。
+    const newTime=+new Date();
+
+    //res.setHeader("cache-control",`public,max-age=${maxAge}`);
+  
+    //协商缓存
+    //Etag / If-None-Match
+    //Last-Modified / If-Modified-Since
+    new Date().toGMTString()
+    const ifNoneMatch=req.header["if-none-match"];
+    const ifModifiedSince=req.header["if-modified-since"]
+    res.setHeader('Cache-Control', 'public, max-age=0');
+    res.setHeader('Last-Modified', new Date().toGMTString());
+    res.setHeader('ETag', "sdjhfjshdjf");
+}
 
 function isHasDefaultShowPage(files){
     return files.includes(defaultShowPage)
@@ -65,15 +171,16 @@ function isNeedCompress(extname){
 
 
 
-function handleCompressFile( req,res,readStream){
+function handleCompressFile(req,res,readStream){
+    console.log("handleCompressFile");
     const acceptEncoding = req.headers["accept-encoding"];
     if(!acceptEncoding){
         return readStream
     } else if(acceptEncoding.includes("gzip")){
-        res.writeHead(200, { 'Content-Encoding': 'gzip' });
+        res.setHeader( 'Content-Encoding', 'gzip' );
         return readStream.pipe(zlib.createGzip())
      }else if(acceptEncoding.includes("deflate")){
-       res.writeHead(200, { 'Content-Encoding': 'deflate' });
+        res.setHeader( 'Content-Encoding', 'deflate' );
        return readStream.pipe(zlib.createDeflate())
      }
 
@@ -95,10 +202,10 @@ module.exports=async (req,res)=>{
     if(stats.isDirectory()){//文件夹操作
         const files =await readdir(process.cwd()+pathName);
         isHasDefaultShowPage(files) 
-        ?handleFile(req,res,path.join(pathName,defaultShowPage))
+        ?handleFile(req,res,path.join(pathName,defaultShowPage),stats)
         :handleDirectory(res,pathName,files)
     }else{ //文件操作
-      handleFile(req, res,pathName)
+      handleFile(req, res,pathName,stats)
     }
      
     
